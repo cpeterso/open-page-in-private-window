5a86fdc5896c89b946d5cbf9bcff05f6492fe1dd
Chris Peterson <cpeterson@mozilla.com>
2020-05-04 22:21:36 -0700

    Open google.com by default

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-web-ext run -f /Applications/FirefoxNightly.app/Contents/MacOS/firefox -s src -v --pre-install
+web-ext run -f /Applications/FirefoxNightly.app/Contents/MacOS/firefox -s src -v --pre-install -u "https://www.google.com"
